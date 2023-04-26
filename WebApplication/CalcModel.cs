b9c4159940a6cb42f3c54753291f1b561a57ba00
xdolak08 <xdolak08@vutbr.cz>
2023-04-26 19:45:18 +0200

    v2

--- WebApplication/CalcModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication
-{
-    public class CalcModel
-    {
-        public decimal Operand1 { get; set; }
-        public decimal Operand2 { get; set; }
-        public string Operation { get; set; }
-    }
-}
